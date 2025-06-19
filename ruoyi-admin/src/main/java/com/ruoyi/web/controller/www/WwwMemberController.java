d207576aa7afd55697aae0a4749cc8f88c53be34
xuexin_419 <305930712@qq.com>
2025-06-19 18:35:05 +0800

    feat:提交代码

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/www/WwwMemberController.java
@@ -0,0 +1,39 @@
+package com.ruoyi.web.controller.www;
+
+import com.ruoyi.common.annotation.RateLimiter;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.project.domain.ProjectMember;
+import com.ruoyi.project.service.IProjectMemberService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 项目成员Controller
+ * 
+ * @author xuexin
+ * @date 2025-06-19
+ */
+@RestController
+@RequestMapping("/www/member")
+public class WwwMemberController extends BaseController
+{
+    @Resource
+    private IProjectMemberService projectMemberService;
+
+    /**
+     * 查询项目成员列表
+     */
+    @GetMapping("/getMenuDataList")
+    @RateLimiter(count = 200, time = 1)
+    public AjaxResult getMenuDataList(){
+        ProjectMember projectMember = new ProjectMember();
+        List<ProjectMember> list = projectMemberService.selectProjectMemberList(projectMember);
+        return AjaxResult.success(list);
+    }
+
+
+}
