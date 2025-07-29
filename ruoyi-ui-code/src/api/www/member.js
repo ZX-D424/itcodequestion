ce6e121b5881164b92ef4ba3397d8dc0f995a255
xuexin_419 <305930712@qq.com>
2025-07-29 14:51:50 +0800

    提交

--- ruoyi-ui-code/src/api/www/member.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/userRequest'
-
-//获取菜单列表下拉框数据
-export function getMemberDataList() {
-  return request({
-    url: '/www/member/getMemberDataList',
-    method: 'get',
-  })
-}
